9c19350f6ef89db70e1fbf81fb781f78eccedfdb
iosif <teratourg@gmail.com>
2026-01-04 20:57:57 +0200

    04/01

--- q1.py
+++ q1.py
@@ -2,3 +2,8 @@ import pandas as pd
 import numpy as np
 import crime_data as cd
 
+vdf = cd.ValTest_df
+def normaldistribution(x, mean, var):
+    return (1 / np.sqrt(2 * np.pi * var)) * np.exp(-0.5 * ((x - mean) ** 2) / var)
+
+print(vdf["hour_float"])
