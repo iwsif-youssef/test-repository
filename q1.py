ae33c38145a92729fa3e77fc07719327b5f7d49f
iosif <teratourg@gmail.com>
2026-01-05 18:10:39 +0200

    05/01

--- q1.py
+++ q1.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import numpy as np
 import crime_data as cd
+import matplotlib.pyplot as plt
 
 vdf = cd.ValTest_df
-def normaldistribution(x, mean, var):
-    return (1 / np.sqrt(2 * np.pi * var)) * np.exp(-0.5 * ((x - mean) ** 2) / var)
 
-print(vdf["hour_float"])
+fig, axs = plt.subplots(2, 2)
+
+axs[0, 0].hist(vdf["hour_float"], bins=24, density=True)
+axs[0, 0].set_title("hours")
+
+axs[0, 1].hist(vdf["latitude"], bins=30, density=True)
+axs[0, 1].set_title("latitude")
+
+axs[1, 0].hist(vdf["longitude"], bins=30, density=True)
+axs[1, 0].set_title("longitude")
+
+axs[1, 1].hist(vdf["victim_age"], bins=30, density=True)
+axs[1, 1].set_title("ictim age")
+
+plt.tight_layout()
+
+plt.show()
